9c1f0ffd36f4fa0c1667f8b4a4463928883a0c30
kinglintianxia <wangliujun_bit@163.com>
2020-12-17 11:20:24 +0800

    add CMakeLists.txt & argv for test.cpp

--- test.cpp
+++ test.cpp
@@ -4,105 +4,110 @@
 using namespace cv;
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {	
-	//***************************** ED Edge Segment Detection *****************************
-	//Detection of edge segments from an input image	
-	Mat testImg = imread("billiard.jpg", 0);	
-	imshow("Source Image", testImg);
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " [img_dir]" << std::endl;
+        return 1;
+    }
 
-	//Call ED constructor
-	ED testED = ED(testImg, SOBEL_OPERATOR, 36, 8, 1, 10, 1.0, true); // apply ED algorithm
-	
-	//Show resulting edge image
-	Mat edgeImg = testED.getEdgeImage();
-	imshow("Edge Image - PRESS ANY KEY TO CONTINUE", edgeImg);
-	waitKey();
-		
-	//Output number of segments
-	int noSegments = testED.getSegmentNo();
-	std::cout << "Number of edge segments: " << noSegments << std::endl;
-		
-	//Get edges in segment form (getSortedSegments() gives segments sorted w.r.t. legnths) 
-	std::vector< std::vector<Point> > segments = testED.getSegments();
-	
-	
-	//***************************** EDLINES Line Segment Detection *****************************
-	//Detection of line segments from the same image
-	EDLines testEDLines = EDLines(testImg);
-	Mat lineImg = testEDLines.getLineImage();	//draws on an empty image
-	imshow("Line Image 1 - PRESS ANY KEY TO CONTINUE", lineImg);
+    //***************************** ED Edge Segment Detection *****************************
+    //Detection of edge segments from an input image
+    Mat testImg = imread(argv[1], 0);
+    imshow("Source Image", testImg);
 
-	//Detection of lines segments from edge segments instead of input image
-	//Therefore, redundant detection of edge segmens can be avoided
-	testEDLines = EDLines(testED);
-	lineImg = testEDLines.drawOnImage();	//draws on the input image
-	imshow("Line Image 2  - PRESS ANY KEY TO CONTINUE", lineImg);
+    //Call ED constructor
+    ED testED = ED(testImg, SOBEL_OPERATOR, 36, 8, 1, 10, 1.0, true); // apply ED algorithm
 
-	//Acquiring line information, i.e. start & end points
-	vector<LS> lines = testEDLines.getLines();
-	int noLines = testEDLines.getLinesNo();
-	std::cout << "Number of line segments: " << noLines << std::endl;
+    //Show resulting edge image
+    Mat edgeImg = testED.getEdgeImage();
+    imshow("Edge Image - PRESS ANY KEY TO CONTINUE", edgeImg);
+    waitKey();
 
-	waitKey();
+    //Output number of segments
+    int noSegments = testED.getSegmentNo();
+    std::cout << "Number of edge segments: " << noSegments << std::endl;
 
-	//************************** EDPF Parameter-free Edge Segment Detection **************************
-	// Detection of edge segments with parameter free ED (EDPF)
+    //Get edges in segment form (getSortedSegments() gives segments sorted w.r.t. legnths)
+    std::vector< std::vector<Point> > segments = testED.getSegments();
 
-	EDPF testEDPF = EDPF(testImg);
-	Mat edgePFImage = testEDPF.getEdgeImage();
-	imshow("Edge Image Parameter Free", edgePFImage);
-	cout << "Number of edge segments found by EDPF: " << testEDPF.getSegmentNo() << endl;
-	waitKey();
+    //***************************** EDLINES Line Segment Detection *****************************
+    //Detection of line segments from the same image
+    EDLines testEDLines = EDLines(testImg);
+    Mat lineImg = testEDLines.getLineImage();	//draws on an empty image
+    imshow("Line Image 1 - PRESS ANY KEY TO CONTINUE", lineImg);
 
-	//***************************** EDCIRCLES Circle Segment Detection *****************************
-	//Detection of circles directly from the input image
+    //Detection of lines segments from edge segments instead of input image
+    //Therefore, redundant detection of edge segmens can be avoided
+    testEDLines = EDLines(testED);
+    lineImg = testEDLines.drawOnImage();	//draws on the input image
+    imshow("Line Image 2  - PRESS ANY KEY TO CONTINUE", lineImg);
 
-	EDCircles testEDCircles = EDCircles(testImg);
-	Mat circleImg = testEDCircles.drawResult(false, ImageStyle::CIRCLES);
-	imshow("Circle Image 1", circleImg);
+    //Acquiring line information, i.e. start & end points
+    vector<LS> lines = testEDLines.getLines();
+    int noLines = testEDLines.getLinesNo();
+    std::cout << "Number of line segments: " << noLines << std::endl;
 
-	//Detection of circles from already available EDPF or ED image
-	testEDCircles = EDCircles(testEDPF);
-	
-	//Get circle information as [cx, cy, r]
-	vector<mCircle> circles = testEDCircles.getCircles();
+    waitKey();
 
-	//Get ellipse information as [cx, cy, a, b, theta]
-	vector<mEllipse> ellipses = testEDCircles.getEllipses();
+    //************************** EDPF Parameter-free Edge Segment Detection **************************
+    // Detection of edge segments with parameter free ED (EDPF)
 
-	//Circles and ellipses will be indicated in green and red, resp.
-	circleImg = testEDCircles.drawResult(true, ImageStyle::BOTH);
-	imshow("CIRCLES and ELLIPSES RESULT IMAGE", circleImg);
+    EDPF testEDPF = EDPF(testImg);
+    Mat edgePFImage = testEDPF.getEdgeImage();
+    imshow("Edge Image Parameter Free", edgePFImage);
+    cout << "Number of edge segments found by EDPF: " << testEDPF.getSegmentNo() << endl;
+    waitKey();
 
-	int noCircles = testEDCircles.getCirclesNo();
-	std::cout << "Number of circles: " << noCircles << std::endl;
-	waitKey();
-	
-	//*********************** EDCOLOR Edge Segment Detection from Color Images **********************
-		
-	Mat colorImg = imread("billiard.jpg");	
-	//Mat colorImg = imread("billiardNoise.jpg");
-	EDColor testEDColor = EDColor(colorImg, 36, 4, 1.5, true); //last parameter for validation
-	imshow("Color Edge Image - PRESS ANY KEY TO QUIT", testEDColor.getEdgeImage());
-	cout << "Number of edge segments detected by EDColor: " << testEDColor.getSegmentNo() << endl;	
-	waitKey();	
-	
-	// get lines from color image
-	EDLines colorLine = EDLines(testEDColor);
-	imshow("Color Line", colorLine.getLineImage());
-	std::cout << "Number of line segments: " << colorLine.getLinesNo() << std::endl;
-	waitKey();
+    //***************************** EDCIRCLES Circle Segment Detection *****************************
+    //Detection of circles directly from the input image
 
-	// get circles from color image
-	EDCircles colorCircle = EDCircles(testEDColor);
-	// TO DO :: drawResult doesnt overlay (onImage = true) when input is from EDColor
-	circleImg = colorCircle.drawResult(false, ImageStyle::BOTH);
-	imshow("Color Circle", circleImg);
-	std::cout << "Number of line segments: " << colorCircle.getCirclesNo() << std::endl;
-	waitKey();
+    EDCircles testEDCircles = EDCircles(testImg);
+    Mat circleImg = testEDCircles.drawResult(false, ImageStyle::CIRCLES);
+    imshow("Circle Image 1", circleImg);
 
-	return 0;
+    //Detection of circles from already available EDPF or ED image
+    testEDCircles = EDCircles(testEDPF);
+
+    //Get circle information as [cx, cy, r]
+    vector<mCircle> circles = testEDCircles.getCircles();
+
+    //Get ellipse information as [cx, cy, a, b, theta]
+    vector<mEllipse> ellipses = testEDCircles.getEllipses();
+
+    //Circles and ellipses will be indicated in green and red, resp.
+    circleImg = testEDCircles.drawResult(true, ImageStyle::BOTH);
+    imshow("CIRCLES and ELLIPSES RESULT IMAGE", circleImg);
+
+    int noCircles = testEDCircles.getCirclesNo();
+    std::cout << "Number of circles: " << noCircles << std::endl;
+    waitKey();
+
+    //*********************** EDCOLOR Edge Segment Detection from Color Images **********************
+
+    Mat colorImg = imread(argv[1]);
+    //Mat colorImg = imread("billiardNoise.jpg");
+    EDColor testEDColor = EDColor(colorImg, 36, 4, 1.5, true); //last parameter for validation
+    imshow("Color Edge Image - PRESS ANY KEY TO QUIT", testEDColor.getEdgeImage());
+    cout << "Number of edge segments detected by EDColor: " << testEDColor.getSegmentNo() << endl;
+    waitKey();
+
+    // get lines from color image
+    EDLines colorLine = EDLines(testEDColor);
+    imshow("Color Line", colorLine.getLineImage());
+    std::cout << "Number of line segments: " << colorLine.getLinesNo() << std::endl;
+    waitKey();
+
+    // get circles from color image
+    EDCircles colorCircle = EDCircles(testEDColor);
+    // TO DO :: drawResult doesnt overlay (onImage = true) when input is from EDColor
+    circleImg = colorCircle.drawResult(false, ImageStyle::BOTH);
+    imshow("Color Circle", circleImg);
+    std::cout << "Number of line segments: " << colorCircle.getCirclesNo() << std::endl;
+    waitKey();
+
+    return 0;
 }
 
 
